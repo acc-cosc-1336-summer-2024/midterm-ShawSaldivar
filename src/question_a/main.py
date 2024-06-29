2c03bf689853de6f04da25a625d12e35312d89b1
ShawSaldivar <Shaw.Saldivar@gmail.com>
2024-06-29 19:45:12 +0000

    Question A SavePoint

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,24 @@
 #add import
+import question_a
+
+
+def main():
+    AnythingElse = 'y'
+    while(question_a.Exit(AnythingElse)):
+        WeekNum = int(input("Please enter a number to display a day of the Week: "))
+        if (WeekNum >= 1 and WeekNum <= 7 ):
+            ConvNum = question_a.Get_Whats_Today(WeekNum)
+            names = question_a.Name_That_Day(ConvNum)
+
+            print("You chose: ", WeekNum,"Which is: ", names)
+            AnythingElse = input("Your question has been answered, do you have another? Say [ Y ] to continue, [ N ] to quit. ")
+            question_a.Exit(AnythingElse)
+
+            if (AnythingElse != "y"):
+                return False
+
+        else:
+            print("Invaid day, please choose a number within the week.")
+            
+
+main()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,33 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+def Get_Whats_Today(day):
+    if (day >= 1 and day <= 7):
+        names = day
+        return names
+    elif (day >= 0 or day < 7):
+        print("Sorry that is not a valid day, enter a name that is in the range of 1 - 7 inclusive.")
+
+def Name_That_Day(names):
+    if (names == 1):
+        return "Monday"
+    elif (names == 2):
+        return "Tuesday"
+    elif (names == 3):
+        return "Wednesday"
+    elif (names == 4):
+        return "Thursday"
+    elif (names == 5):
+        return "Friday"
+    elif (names == 6):
+        return "Saturday"
+    elif (names == 7):
+        return "Sunday"
+    
+def Exit(Answer):
+    ReallyReally = Answer
+    if ReallyReally == "y":
+        return True
+    elif ReallyReally == "n":
+        return False
