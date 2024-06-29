a0430e54d134381bd1a9c6c51a496b2eddc4f93b
ShawSaldivar <Shaw.Saldivar@gmail.com>
2024-06-29 20:57:36 +0000

    Question B Save Point

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,13 @@
 #add import
+import question_b
+
+def main():
+    Kill = int(input("Please enter Kilometers: " ))
+    Minoot = int(input("Please Enter your Minutes: "))
+
+    if (Kill >= 0 and Minoot >= 0):
+        Flash = question_b.get_miles_per_hour(Kill, Minoot)
+        print("Your speed will be: ", Flash, "Miles Per Hour")
+    else:
+        print("Invalid speed.")
+main()
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,6 @@
 #write functions here, don't add input('') statements here!
+
+def get_miles_per_hour(Kilometers, Minutes):
+   KilPerHour = Kilometers / Minutes
+   AMilPerHour = Kilometers * .621371
+   return AMilPerHour
